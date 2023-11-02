27a694f646852662f8db7b7ab4a94a1f8f7e378c
alpolcaymis <alpmis99@gmail.com>
2023-11-02 15:40:19 +0300

    playerName editable now

---
+++ src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    //based on previous value of state
+    //u should pass function
+    //this is strong recommendation by React Developers
+    setIsEditing((editing) => !editing);
+  }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = "Save";
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
