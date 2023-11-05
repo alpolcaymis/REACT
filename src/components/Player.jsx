d489e2a6c044010cd06349290da80105d0ae1dad
alpolcaymis <alpmis99@gmail.com>
2023-11-05 15:56:52 +0300

    tictactoe project finished

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,34 +1,43 @@
-import { useState } from "react";
-export default function Player({ initialName, symbol }) {
+import { useState } from 'react';
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    //based on previous value of state
-    //u should pass function
-    //this is strong recommendation by React Developers
     setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
+
   function handleChange(event) {
     setPlayerName(event.target.value);
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
-  // let btnCaption = "Edit";
+  // let btnCaption = 'Edit';
 
   if (isEditing) {
     editablePlayerName = (
       <input type="text" required value={playerName} onChange={handleChange} />
     );
-    // btnCaption = "Save";
+    // btnCaption = 'Save';
   }
+
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
