b456e5356348c562e9e8b78fc99c3c3a537b3fe2
alpolcaymis <alpmis99@gmail.com>
2023-12-04 01:58:15 +0300

    yoruldum

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,5 +1,5 @@
+import icon from "../assets/send.png";
 export default function Places({ name, places, fallbackText, onSelectPlace }) {
-  const img2 = "../assests/send2.png";
   return (
     <section className="places-category">
       <h2>{name}</h2>
@@ -13,10 +13,7 @@ export default function Places({ name, places, fallbackText, onSelectPlace }) {
                 <h3>{place.name}</h3>
                 <h4>{place.borough}</h4>
                 <a href={place.location_link} target="_blank">
-                  <img
-                    src="./src/assets/send.png"
-                    className="icon-direction"
-                  ></img>
+                  <img src={icon} className="icon-direction"></img>
                 </a>
               </button>
             </li>
