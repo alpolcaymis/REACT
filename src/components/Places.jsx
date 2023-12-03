2a570f09f3cc2a05a50ab6e5855eb6083a7a56d7
alpolcaymis <alpmis99@gmail.com>
2023-12-03 21:20:55 +0300

    3 aralık 20213

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,4 +1,5 @@
 export default function Places({ name, places, fallbackText, onSelectPlace }) {
+  const img2 = "../assests/send2.png";
   return (
     <section className="places-category">
       <h2>{name}</h2>
@@ -8,8 +9,15 @@ export default function Places({ name, places, fallbackText, onSelectPlace }) {
           {places.map((place) => (
             <li key={place.cid} className="place-item">
               <button onClick={() => onSelectPlace(place.cid)}>
-                <img src={place.photo} />
+                <img src={place.photo} alt="" className="photo" />
                 <h3>{place.name}</h3>
+                <h4>{place.borough}</h4>
+                <a href={place.location_link} target="_blank">
+                  <img
+                    src="src/assets/send.png"
+                    className="icon-direction"
+                  ></img>
+                </a>
               </button>
             </li>
           ))}
