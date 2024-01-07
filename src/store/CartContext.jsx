ed97b5d6955ad7facdde1a9af008f9a281c95dca
alpolcaymis <alpmis99@gmail.com>
2024-01-07 16:40:24 +0300

    7 jan 24

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer } from "react";
 
 const CartContext = createContext({
   items: [],
@@ -7,7 +7,7 @@ const CartContext = createContext({
 });
 
 function cartReducer(state, action) {
-  if (action.type === 'ADD_ITEM') {
+  if (action.type === "ADD_ITEM") {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
@@ -28,7 +28,7 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
-  if (action.type === 'REMOVE_ITEM') {
+  if (action.type === "REMOVE_ITEM") {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
     );
@@ -56,17 +56,17 @@ export function CartContextProvider({ children }) {
   const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
   function addItem(item) {
-    dispatchCartAction({ type: 'ADD_ITEM', item });
+    dispatchCartAction({ type: "ADD_ITEM", item });
   }
 
   function removeItem(id) {
-    dispatchCartAction({ type: 'REMOVE_ITEM', id });
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
   }
 
   const cartContext = {
     items: cart.items,
     addItem,
-    removeItem
+    removeItem,
   };
 
   return (
