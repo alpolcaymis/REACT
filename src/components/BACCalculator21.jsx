c60abbe8d97600c46d36988e92939b6d6ea3364c
alpolcaymis <alpmis99@gmail.com>
2024-01-15 22:05:41 +0300

    sadas

--- src/components/BACCalculator21.jsx
+++ src/components/BACCalculator21.jsx
@@ -152,164 +152,191 @@ const BACCalculator21 = () => {
 
   return (
     <div>
-      <h2>Blood Alcohol Concentration Calculator</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <h3>Personal Information</h3>
-          <label>
-            Gender:
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  id="gender-man"
-                  value="man"
-                  checked={gender === "man"}
-                  onChange={() => setGender("man")}
-                />
-                Man
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  id="gender-female"
-                  value="woman"
-                  checked={gender === "woman"}
-                  onChange={() => setGender("woman")}
-                />
-                Woman
-              </label>
-            </div>
-          </label>
-          <br />
-          <label>
-            Height (cm):
-            <input
-              type="number"
-              id="height"
-              min="110"
-              max="230"
-              value={height}
-              onChange={(e) => setHeight(e.target.value)}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Weight (kg):
-            <input
-              type="number"
-              id="weight"
-              min="40"
-              max="250"
-              value={weight}
-              onChange={(e) => setWeight(e.target.value)}
-              required
-            />
-          </label>
-        </div>
-
-        {drinks.map((drink, index) => (
-          <div key={drink.id}>
-            <h3>Drink #{index + 1}</h3>
-            <label>
-              Type:
-              <select
-                id={`type-${drink.id}`}
-                value={drink.type}
-                onChange={(e) => handleTypeChange(drink.id, e.target.value)}
-                required
-              >
-                <option value="beer">Beer</option>
-                <option value="wine">Wine</option>
-                <option value="distilled">Distilled</option>
-                <option value="liqueurs">Liqueurs</option>
-                {/* Add more options for other drink types as needed */}
-              </select>
-              {drinkTypeIcons[drink.type]}
-            </label>
-            <br />
-            <label>
-              Amount:
+      <form className="" onSubmit={handleSubmit}>
+        <section className="gender-section ">
+          <p className="text-center">Gender</p>
+          <div className="flex justify-around">
+            <label className="border p-4 rounded-md">
               <input
-                type="number"
-                id={`amount-${drink.id}`}
-                min="0"
-                max="20"
-                value={drink.amount}
-                onChange={(e) =>
-                  handleInputChange(drink.id, "amount", e.target.value)
-                }
-                required
+                type="radio"
+                id="gender-man"
+                value="man"
+                checked={gender === "man"}
+                onChange={() => setGender("man")}
               />
+              Man
             </label>
-            <label>
-              Volume:
+            <label className="border p-4 rounded-md">
               <input
-                type="number"
-                id={`volume-${drink.id}`}
-                min="1"
-                max="1000"
-                value={drink.volume}
-                onChange={(e) =>
-                  handleInputChange(drink.id, "volume", e.target.value)
-                }
-                required
+                className=""
+                type="radio"
+                id="gender-female"
+                value="woman"
+                checked={gender === "woman"}
+                onChange={() => setGender("woman")}
               />
+              Woman
             </label>
-            <label>
-              Percentage:
-              <input
-                type="number"
-                id={`percentage-${drink.id}`}
-                min="0"
-                max="100"
-                value={drink.percentage}
-                onChange={(e) =>
-                  handleInputChange(drink.id, "percentage", e.target.value)
-                }
-                required
-              />
-            </label>
-            <button type="button" onClick={() => handleDeleteDrink(drink.id)}>
-              Delete Drink
-            </button>
           </div>
-        ))}
-        <div>
-          <h3>Additional Information</h3>
-          <label>
-            How many hours have passed since the first drink?
-            <input
-              type="number"
-              id="hours"
-              min="0"
-              value={hoursPassed}
-              onChange={(e) => setHoursPassed(e.target.value)}
-              required
-            />
-          </label>
-        </div>
-        <br />
-        <button type="button" onClick={handleAddDrink}>
-          Add Drink
-        </button>
-        <br />
-        <button type="submit" className="button">
-          Calculate BAC
-        </button>
-        {renderResult("Blood Volume (ml)", bloodVolume.toFixed(2))}
-        {renderResult("Total Alcohol Millimeter", totalAlcoholMillimeter)}
-        {renderResult("Total Alcohol Milligram", totalAlcoholMilligram)}
-        {renderResult(
-          "Calculated BAC Before Deduction (per 100 milliliters)",
-          calculatedBACBeforeDeduction.toFixed(2)
-        )}
-        {renderResult(
-          "Calculated BAC After Deduction (per 100 milliliters)",
-          calculatedBACAfterDeduction.toFixed(2)
-        )}
+        </section>
+
+        <section className="hw-section  ">
+          <p className="text-center">Physical Apperance</p>
+          <div className="flex justify-around">
+            <label>
+              Height (cm):
+              <input
+                type="number"
+                id="height"
+                min="110"
+                max="230"
+                value={height}
+                onChange={(e) => setHeight(e.target.value)}
+                required
+              />
+            </label>
+            <label>
+              Weight (kg):
+              <input
+                type="number"
+                id="weight"
+                min="40"
+                max="250"
+                value={weight}
+                onChange={(e) => setWeight(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+        </section>
+
+        <section className="drinks-section ">
+          <p className="text-center">Drinks section</p>
+          <div className=" ">
+            {drinks.map((drink, index) => (
+              <div className="drink-item" key={drink.id}>
+                <label>
+                  Type:
+                  <select
+                    id={`type-${drink.id}`}
+                    value={drink.type}
+                    onChange={(e) => handleTypeChange(drink.id, e.target.value)}
+                    required
+                  >
+                    <option value="beer">Beer</option>
+                    <option value="wine">Wine</option>
+                    <option value="distilled">Distilled</option>
+                    <option value="liqueurs">Liqueurs</option>
+                    {/* Add more options for other drink types as needed */}
+                  </select>
+                  {drinkTypeIcons[drink.type]}
+                </label>
+                <label>
+                  Amount:
+                  <input
+                    type="number"
+                    placeholder="amount"
+                    id={`amount-${drink.id}`}
+                    min="0"
+                    max="20"
+                    value={drink.amount}
+                    onChange={(e) =>
+                      handleInputChange(drink.id, "amount", e.target.value)
+                    }
+                    required
+                  />
+                </label>
+                <label>
+                  Volume:
+                  <input
+                    type="number"
+                    placeholder="volume mL"
+                    id={`volume-${drink.id}`}
+                    min="1"
+                    max="1000"
+                    value={drink.volume}
+                    onChange={(e) =>
+                      handleInputChange(drink.id, "volume", e.target.value)
+                    }
+                    required
+                  />
+                </label>
+                <label>
+                  Percentage:
+                  <input
+                    type="number"
+                    placeholder="Percentage"
+                    id={`percentage-${drink.id}`}
+                    min="0"
+                    max="100"
+                    value={drink.percentage}
+                    onChange={(e) =>
+                      handleInputChange(drink.id, "percentage", e.target.value)
+                    }
+                    required
+                  />
+                </label>
+                <label>
+                  <button
+                    className="p-1 border rounded-lg"
+                    type="button"
+                    onClick={() => handleDeleteDrink(drink.id)}
+                  >
+                    Delete
+                  </button>
+                </label>
+              </div>
+            ))}
+          </div>
+          <button
+            className="p-2 bg-yellow-800"
+            type="button"
+            onClick={handleAddDrink}
+          >
+            Add Drink
+          </button>
+        </section>
+
+        <section className="additional-section border">
+          <p className="text-center">Additional Information</p>
+          <div className="">
+            <label>
+              How many hours have passed since the first drink?
+              <input
+                type="number"
+                id="hours"
+                min="0"
+                value={hoursPassed}
+                onChange={(e) => setHoursPassed(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+        </section>
+
+        <section className="calculate-button-section border">
+          <button type="submit" className="button">
+            Calculate BAC
+          </button>
+        </section>
+
+        <section className="results-section border">
+          <p className="text-center">Result Section</p>
+          <div className="">
+            {renderResult("Blood Volume (ml)", bloodVolume.toFixed(2))}
+            {renderResult("Total Alcohol Millimeter", totalAlcoholMillimeter)}
+            {renderResult("Total Alcohol Milligram", totalAlcoholMilligram)}
+            {renderResult(
+              "Calculated BAC Before Deduction (per 100 milliliters)",
+              calculatedBACBeforeDeduction.toFixed(2)
+            )}
+            {renderResult(
+              "Calculated BAC After Deduction (per 100 milliliters)",
+              calculatedBACAfterDeduction.toFixed(2)
+            )}
+          </div>
+        </section>
+        {""}
       </form>
     </div>
   );
