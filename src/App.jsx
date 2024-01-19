eaa6c93e51b81528dfe45fd0acc447858eae4a49
alpolcaymis <alpmis99@gmail.com>
2024-01-19 18:26:57 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,30 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { EXAMPLES } from "./data.js";
+import Counter from './components/Counter/Counter.jsx';
+import Header from './components/Header.jsx';
+import { log } from './log.js';
+import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
+  log('<App /> rendered');
 
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-    // console.log(selectedTopic);
-  }
+  const [chosenCount, setChosenCount] = useState(0);
 
-  console.log("APP COMPONENT EXECUTING");
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
+  function handleSetCount(newCount) {
+    setChosenCount(newCount);
+    setChosenCount((prevChosenCount) => prevChosenCount + 1);
+    console.log(chosenCount); // won't work!
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-
-            {/* to see spread operator effect on object */}
-            {CORE_CONCEPTS.map((conceptItem) =>
-              console.log({ ...conceptItem })
-            )}
-            {CORE_CONCEPTS.map((conceptItem) => console.log({ conceptItem }))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => handleSelect("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <ConfigureCounter onSet={handleSetCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
+        <Counter initialCount={0} />
       </main>
-    </div>
+    </>
   );
 }
 
