1925616751ee22d492ee60c7d5547971a1117719
alpolcaymis <alpmis99@gmail.com>
2024-06-27 20:28:23 +0300

    sad

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 export default function Header() {
   return (
     <>
-      <h1 className="font-bold text-[1.2rem] text-center mt-16 uppercase">
-        Online alkolmetre promil hesaplama
+      <h1 className="font-bold text-[1.2rem] text-center mt-16 mb-2 uppercase">
+        promil hesaplama online alkolmetre ölçer
       </h1>
       <h2 className="font-thin text-[0.8rem] text-center text-black uppercase font-sans font-bold text-3xl tracking-widest text-[#2f4444]">
-        Test Doğruluk oranı 98,87 %
+        Doğruluk oranı en yüksek test
       </h2>
-      <p className="text-[0.7rem] text-center text-black uppercase">
-        (sapma hata payı 1,12 %)
+      <p className="text-[0.4rem] text-center text-black uppercase">
+        ("Bu ölçümler veri bazlı tahminlere dayanmaktadır ve herhangi bir
+        kesinlik teşkil etmemektedir. Buradaki verilere göre hareket ederek,
+        kendinizin ve etrafınızdakilerin can güvenliğini tehlikeye atmayınız
+        ❗")
       </p>
     </>
   );
