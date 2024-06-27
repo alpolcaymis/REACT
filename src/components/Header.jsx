f39c84872dbbd8f1d3fe2668099716e1408e50dc
alpolcaymis <alpmis99@gmail.com>
2024-06-27 22:36:07 +0300

    ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       <h1 className="font-bold text-[1.2rem] text-center mt-16 mb-2 uppercase">
         promil hesaplama online alkolmetre ölçer
       </h1>
-      <h2 className="font-thin text-[0.8rem] text-center text-black uppercase font-sans font-bold text-3xl tracking-widest text-[#2f4444]">
+      <h2 className="font-normal text-[0.8rem] text-center uppercase font-sans font-bold text-3xl tracking-widest text-[#2f4444]">
         yüksek Doğruluk oranlı en gerçekçi test
       </h2>
       <p className="text-[0.4rem] text-center text-black uppercase">
